0c99ff25756281b9ec9beb2bf6396d4244c829e1
Yuriko Inaba <yurikoainaba@gmail.com>
2025-02-25 15:32:59 -0500

    Update Zend to 5.0.6 (#57)

--- ci/devtools/bump_version.sh
+++ ci/devtools/bump_version.sh
@@ -2,8 +2,8 @@
 
 set -eEuox pipefail
 
-ZEND_VERISON_OLD="${ZEND_VERISON_OLD:-5.0.4}"
-ZEND_VERSION_NEW="${ZEND_VERSION_NEW:-5.0.5}"
+ZEND_VERISON_OLD="${ZEND_VERISON_OLD:-5.0.5}"
+ZEND_VERSION_NEW="${ZEND_VERSION_NEW:-5.0.6}"
 
 bsd_sed=""
 [ "$(uname -s)" = "Darwin" ] && bsd_sed=" ''"
