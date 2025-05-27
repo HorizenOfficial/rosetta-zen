8bdc9c4b36fcce9528e29bc03064d63f0e6a44d6
Yuriko Inaba <yurikoainaba@gmail.com>
2025-05-27 13:19:10 -0400

    Update Zend to 5.0.7 (#58)

--- services/network_service_test.go
+++ services/network_service_test.go
@@ -30,7 +30,7 @@ var (
 	defaultNetworkOptions = &types.NetworkOptionsResponse{
 		Version: &types.Version{
 			RosettaVersion:    types.RosettaAPIVersion,
-			NodeVersion:       "5.0.6",
+			NodeVersion:       "5.0.7",
 			MiddlewareVersion: &middlewareVersion,
 		},
 		Allow: &types.Allow{
