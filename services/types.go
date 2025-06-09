f3d31316aa4b1749aa2319124c5570500f32e05c
cronicc <cronic@zensystem.io>
2025-06-09 15:26:10 +0200

    Update Zend to 6.0.0-rc1 (#59)

--- services/types.go
+++ services/types.go
@@ -24,7 +24,7 @@ import (
 const (
 	// NodeVersion is the version of
 	// zend core we are using.
-	NodeVersion = "5.0.7"
+	NodeVersion = "6.0.0-rc1"
 
 	// HistoricalBalanceLookup indicates
 	// that historical balance lookup is supported.
